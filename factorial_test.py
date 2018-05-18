1562209766078eaa386cf1d3b1774adb3ee4a8ae
Shishakt Kumar <shishaktasct@gmail.com>
2018-05-18 15:16:23 +0530

    My first commit

---
+++ factorial_test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/ython
+from nose.tools import *
+import unittest
+from factorial import fact
+
+class TestFactorial(unittest.TestCase):
+    def test_fact(self):
+        res = fact(5)
+        self.assertEqual(res, 120)
+if __name__ == '__main__':
+    unittest.main()
