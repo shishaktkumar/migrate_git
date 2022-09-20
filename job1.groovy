f5e2efff106bbc59ca21f2267d6ab1cf317d70bd
shishakt <42597947+shishakt@users.noreply.github.com>
2022-09-20 14:40:28 +0530

    Update job1.groovy

--- job1.groovy
+++ job1.groovy
@@ -13,6 +13,30 @@ job('Job1') {
          BRANCH : 'master',
          REPO_ALIAS : 'delphix_task'
     )
+    groovy ('''
+groovy ('''
+import hudson.*
+import jenkins.*
+import jenkins.model.*
+import hudson.model.*
+import com.cloudbees.plugins.credentials.*;
+import com.cloudbees.plugins.credentials.domains.Domain;
+import org.jenkinsci.plugins.plaincredentials.impl.FileCredentialsImpl;
+
+def jenkinsCredentials = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
+        com.cloudbees.plugins.credentials.Credentials.class,
+        Jenkins.instance,null,null);
+
+def mapList=[:]
+for (creds in jenkinsCredentials) {
+  if(creds.id == "GITHUB"){
+        mapList['GITHUB_USERNAME']= creds.username.toString()
+        mapList['GITHUB_PASSWORD']= creds.password.toString()
+    }
+}
+
+return(mapList)
+''')
   }
   label('master')
 
@@ -52,7 +76,9 @@ tar -cvf artifacts.tar *
 mv job1.groovy job1_${BUILD_NUMBER}.groovy
 echo buildnumber=${BUILD_NUMBER} > prop.properties
 ''')
-    
+    environmentVariables {
+      propertiesFile('prop.properties')
+    }
   }
    
    publishers {
@@ -61,6 +87,20 @@ echo buildnumber=${BUILD_NUMBER} > prop.properties
         pattern('artifacts.tar')
         defaultExcludes(defaultExcludes = true)
         onlyIfSuccessful()
+      }
+    }
+    postBuildScripts {
+      steps {
+        shell('''#!/bin/bash
+REPO_NAME=$(echo $GIT_REPO | cut -d'/' -f5 | cut -d'.' -f1)
+MOD_REPO=$(echo $GIT_REPO | cut -d'/' -f3-)
+
+PUSH_REPO="https://${GITHUB_USERNAME}:${GITHUB_PASSWORD}@${MOD_REPO}"
+git pull ${PUSH_REPO} ${BRANCH}
+git add job1.groovy_${buildnumber}
+git commit -m"Updating file from jenkins job" --no-verify
+git push ${PUSH_REPO} ${BRANCH}
+''')
       }
     }
    }
